9f8b9aebc48aed5041db47416d10539ee2287532
setayeshnri <setayeshn1382@gmail.com>
2024-07-25 15:25:37 +0330

    Game map was added

---
+++ src/kaboomCtx.ts
@@ -0,0 +1,9 @@
+import kaboom from "kaboom";
+import { scale } from "./constants";
+export const k = kaboom({
+  width: 256 * scale,
+  height: 144 * scale,
+  scale,
+  letterbox: true,
+  global: false,
+});
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,28 @@
-import kaboom from "kaboom";
+import { k } from "./kaboomCtx";
 
-export const k = kaboom({
-   
-})
+async function gameSetup() {
+  k.loadSprite("assets", "./kirby-like.png", {
+    sliceX: 9,
+    sliceY: 10,
+    anims: {
+      kirbIdle: 0,
+      kirbInhaling: 1,
+      kirbFull: 2,
+      kirbInhaleEffect: { from: 3, to: 8, speed: 15, loop: true },
+      shootingStar: 9,
+      flame: { from: 36, to: 37, speed: 4, loop: true },
+      guyIdle: 18,
+      guyWalk: { from: 18, to: 19, speed: 4, loop: true },
+      bird: { from: 27, to: 28, speed: 4, loop: true },
+    },
+  });
+  k.loadSprite("level-1", "./level-1.png");
+  k.scene("level-1", () => {
+    k.setGravity(2100);
+    k.add([k.rect(k.width(), k.height()), k.color(k.Color.fromHex("#f7d7db")),
+        k.fixed(),
+    ]);
+  });
+  k.go("level-1");
+}
+gameSetup();
